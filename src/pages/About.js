c4da2822eebec32a7b760ff9ae17019f9e4ee5c3
Jash <100846378+JASH-PATEL-6566@users.noreply.github.com>
2022-07-24 21:01:56 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nisi neque a iure harum dignissimos, similique porro iste temporibus ullam consequatur sit debitis nam dicta. Rerum in nemo distinctio quaerat eligendi!
+      </p>
+    </section>
+  )
+}
+
+export default About
